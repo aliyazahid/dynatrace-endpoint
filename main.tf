6cd1a0254fe55eb88efcfe09329d680f5d949e37
danish125 <drehman073@gmail.com>
2024-11-14 17:26:51 +0500

    dynatrace work

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "aws_route53_record" "dynatrace" {
   alias {
     name                   = aws_vpc_endpoint.replace_me.dns_name
     zone_id                = aws_vpc_endpoint.replace_me.hosted_zone_id
-    # evaluate_target_health = true
+    evaluate_target_health = true
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "tenant_id" {
     type = string
+    description = "Your Dynatrace Environment ID (8 characters)"
 }
 variable "vpc_id" {
     type = string
+    description = "VPC ID of your VPC where PrivateLink was created (must be the same region as your Dynatrace Environment)"
 }
 variable "region" {
   type = string
